26632a7eea6af5ec730d7762fa8462b5b8a117e0
Diana Holland <holla@bikefarm.org>
2020-02-20 17:06:50 -0800

    Add new built files

---
+++ precache-manifest.b2629f54a6aa94688738905e0827f42c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1916c1d15c2a31c9a665bcca0cc78e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af8af798b0c5dc089a51",
+    "url": "/static/css/main.a34ef5e0.chunk.css"
+  },
+  {
+    "revision": "33425b11621a5850a469",
+    "url": "/static/js/2.8793b336.chunk.js"
+  },
+  {
+    "revision": "860bc5081fcd60d3c959d0d8166dd439",
+    "url": "/static/js/2.8793b336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8af798b0c5dc089a51",
+    "url": "/static/js/main.6aecbf2d.chunk.js"
+  },
+  {
+    "revision": "4fe395d315c7047a6922",
+    "url": "/static/js/runtime-main.5fe140e6.js"
+  }
+]);
